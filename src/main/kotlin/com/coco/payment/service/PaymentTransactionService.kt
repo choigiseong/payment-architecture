14f83dd68e9c78427b45860ed0bfc374bdb755fa
choigiseong <gisungcu@gmail.com>
2026-08-09 00:19:16 +0900

    fix: 결제거래 상태 변경 트랜잭션 적용

--- src/main/kotlin/com/coco/payment/service/PaymentTransactionService.kt
+++ src/main/kotlin/com/coco/payment/service/PaymentTransactionService.kt
@@ -4,23 +4,27 @@ import com.coco.payment.persistence.enumerator.PaymentTransactionStatus
 import com.coco.payment.persistence.model.PaymentTransaction
 import com.coco.payment.persistence.repository.PaymentTransactionRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class PaymentTransactionService(private val paymentTransactionRepository: PaymentTransactionRepository) {
     fun findByPaymentKey(paymentKey: String) = paymentTransactionRepository.findByPaymentKey(paymentKey)
 
+    @Transactional
     fun createPending(paymentKey: String, orderId: Long, moid: String, amount: Long): Long {
         val transaction = PaymentTransaction(null, paymentKey, orderId, moid, null, amount, PaymentTransactionStatus.PENDING, null, null)
         check(paymentTransactionRepository.insert(transaction) == 1) { "Failed to insert payment transaction" }
         return transaction.id!!
     }
 
+    @Transactional
     fun complete(paymentTransactionId: Long, tid: String) {
         check(paymentTransactionRepository.mark(paymentTransactionId, PaymentTransactionStatus.PENDING, PaymentTransactionStatus.SUCCESS, tid) == 1) {
             "Failed to mark payment transaction as successful"
         }
     }
 
+    @Transactional
     fun fail(paymentTransactionId: Long) {
         check(paymentTransactionRepository.mark(paymentTransactionId, PaymentTransactionStatus.PENDING, PaymentTransactionStatus.FAILED, null) == 1) {
             "Failed to mark payment transaction as failed"
